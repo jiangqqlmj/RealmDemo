f3bfa87b29d674acb6e0cff8f70fc1118c9a3a51
jiangqqlmj <781931404@qq.com>
2016-05-29 17:48:33 +0800

    完成表结构创建实例

--- realm_models.js
+++ realm_models.js
@@ -10,45 +10,50 @@ import {
   Text,
   View
 } from 'react-native';
-const Realm=require('realm')
+const Realm=require('realm');
 class RealmModelsDemo extends Component {
   render() {
-    let realm = new Realm({
-     schema: [{name: 'Dog', properties: {name: 'string'}}]
-     });
-
-    realm.write(() => {
-      realm.create('Dog', {name: 'Rex'});
+    //创建Person模型 cars:{type:'list',objectType:'Car'},
+    const PersonSchema={
+      name:'Person',
+      properties:{
+        name:'string',
+        nickname:'string',
+        birthday:'date',
+        picture:{type:'string',optional:true}
+      }
+    };
+    //进行初始化realm
+    let realm=new Realm({schema:[PersonSchema]});
+    //进行写数据到表中
+    //1.首先写入Car数据
+    realm.write(()=>{
+        let person=realm.create('Person',{
+          name:'张三',
+          nickname:'我是昵称',
+          picture:'http://www.lcode.org',
+          birthday:new Date(1995, 11, 25),
+        });
     });
+    let person=realm.objects('Person')[0];
     return (
-      <View style={styles.container}>
+      <View style={{marginTop:20}}>
         <Text style={styles.welcome}>
            Realm基础使用实例
         </Text>
-        <Text style={styles.instructions}>
-           Count of Dogs in Realm: {realm.objects('Dog').length}
-        </Text>
+        <Text>数据库中存储的数据为:</Text>
+        <Text>name:{person.name}</Text>  
+        <Text>nickname:{person.nickname}</Text>  
+        <Text>picture:{person.picture}</Text>      
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
